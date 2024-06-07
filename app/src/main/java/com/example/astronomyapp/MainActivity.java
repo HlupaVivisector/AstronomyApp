1ee72e0bc0c557b1463df694989010e11b58292a
HlupaVivisector <s.muniroff2018@yandex.ru>
2024-06-07 15:41:38 +0500

    Initial commit

---
+++ app/src/main/java/com/example/astronomyapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.astronomyapp;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    List<DataClass> dataList;
+    MyAdapter adapter;
+    DataClass androidData;
+    SearchView searchView;
+    Button feedback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        searchView = findViewById(R.id.search);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchList(newText);
+                return true;
+            }
+        });
+
+        feedback =(Button) findViewById(R.id.feedback);
+        feedback.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                String UriText = "mailto:" +      Uri.encode("s.muniroff2018@yandex.ru") +"?subject="+
+                        Uri.encode("Отзывы и предложения") ;
+                Uri uri = Uri.parse(UriText);
+                intent.setData(uri);
+                startActivity(Intent.createChooser(intent,"Обратная связь"));
+            }
+        });
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, 1);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        dataList = new ArrayList<>();
+        androidData = new DataClass("Солнце", R.string.solar, " ", R.drawable.solar_detail);
+        dataList.add(androidData);
+        androidData = new DataClass("Земля", R.string.earth, " ", R.drawable.earth_detail);
+        dataList.add(androidData);
+        androidData = new DataClass("Черная дыра", R.string.black_hole, " ", R.drawable.black_hole_detail);
+        dataList.add(androidData);
+        androidData = new DataClass("Вслененная", R.string.universe, " ", R.drawable.universe_detail);
+        dataList.add(androidData);
+        androidData = new DataClass("Луна", R.string.moon, " ", R.drawable.moon_detail);
+        dataList.add(androidData);
+        adapter = new MyAdapter(MainActivity.this, dataList);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    private void searchList(String text) {
+        List<DataClass> dataSearchList = new ArrayList<>();
+        for (DataClass data : dataList) {
+            if (data.getDataTitle().toLowerCase().contains(text.toLowerCase())) {
+                dataSearchList.add(data);
+            }
+        }
+        if (dataSearchList.isEmpty()) {
+            Toast.makeText(this, "Not Found", Toast.LENGTH_SHORT).show();
+        } else {
+            adapter.setSearchList(dataSearchList);
+        }
+    }
+
+}
+
